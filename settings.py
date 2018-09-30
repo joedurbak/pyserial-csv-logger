17ffa1be1b987bfe29f5ad1effa159669badc800
joedurbak <durbak.3@gmail.com>
2018-09-30 18:04:03 -0400

    updated settings to match htsensor and added bat file to run logger

--- settings.py
+++ settings.py
@@ -2,4 +2,6 @@ COM_PORT = 'COM5'
 BAUD = 9600
 OUTPUT_FILE_PREFIX = "test"
 OUTPUT_FILE_DIR = ""
-CSV_HEADERS = ['row', 'humidity', 'temperature', 'dew_point', ]
+CSV_HEADERS = [
+    'row', 'humidity', 'temperature', 'dew_point', 'frost_point', 'temp_change_flag', 'dew_flag', 'frost_flag'
+]
